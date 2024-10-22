9f765884c055222b4eb83a0cd7d6f473a4ff0db5
albert <albert.mackel@gmail.com>
2024-10-22 21:58:41 +0300

    new folder ci_cd24 learning terraform azure

---
+++ ci_cd24/learn-terraform-azure/main.tf
@@ -0,0 +1,92 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "polandcentral"
+}
+resource "azurerm_storage_account" "alberttfstorage" {
+  name                     = "alberttfstorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tfstorage" {
+  name                  = "tfstorage"
+  storage_account_name  = azurerm_storage_account.alberttfstorage.name
+  container_access_type = "private"
+}
+
+resource "azurerm_virtual_network" "alberttfnetwork" {
+  name                = "tfnetwork"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "alberttfsubnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.alberttfnetwork.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "tfinterface" {
+  name                = "tfinterface1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.alberttfsubnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
+resource "azurerm_linux_virtual_machine" "tfvm3" {
+  name                = "tfvma3"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "albert"
+  network_interface_ids = [
+    azurerm_network_interface.tfinterface.id,
+  ]
+  
+  priority = "Spot"
+
+  eviction_policy = "Deallocate"
+
+  admin_ssh_key {
+    username   = "albert"
+    public_key = file("~/.ssh/disposable.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
