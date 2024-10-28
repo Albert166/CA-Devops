ba6b602e5ac353e2cf2c901565f3aa2b982a9167
albert <albert.mackel@gmail.com>
2024-10-28 20:27:49 +0200

    changed backend file name to save to cloud

--- 27-terraform-workspace/backend.tf
+++ 27-terraform-workspace/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "myTFResourceGroup"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "alberttfstorage"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstorage"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    key                  = "terraform27.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
