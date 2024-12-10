5588f0cefe086c32a2c6e1aa64167db19063662d
albert <albert.mackel@gmail.com>
2024-12-10 21:02:39 +0200

    44 curl api commands for github

--- 44_curl_api/test_token.sh
+++
@@ -1,4 +0,0 @@
-curl --request GET \
---url "https://api.github.com/repos/octocat/Spoon-Knife/issues" \
---header "Accept: application/vnd.github+json" \
---header "Authorization: Bearer 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"
