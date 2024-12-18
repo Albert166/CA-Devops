7397b68c5449a393f1028db60ec5d70aa16672b2
albert <albert.mackel@gmail.com>
2024-12-18 21:00:51 +0200

    fix from checkov #1

--- 48_terraform_checkov/main.tf
+++ 48_terraform_checkov/main.tf
@@ -58,21 +58,21 @@ resource "aws_security_group" "https" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["88.118.178.202/32"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["88.118.178.202/32"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["88.118.178.202/32"]
   }
 
   tags = {
@@ -88,7 +88,7 @@ resource "aws_security_group" "ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["88.118.178.202/32"]
   }
   tags = {
     Name = "ssh-security-group-${terraform.workspace}"
